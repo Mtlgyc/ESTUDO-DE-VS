a71ef8ff18ec26755b20ddc10729114e5acdc6ac
Mtlgyc <caducesd@gmail.com>
2024-09-19 10:50:38 -0300

    Professor.java

--- Professor.java
+++ Professor.java
@@ -1,45 +1,42 @@
 package Sistema;
 
 public class Professor extends Usuario {
-    // Construtor da classe Professor
+
     public Professor(String nome) {
-        super(nome); // Chama o construtor da classe base Usuario para inicializar o nome
+        super(nome); 
     }
 
-    // Método para bloquear um livro
+   
     public boolean bloqueiaLivro(Livro it, int prazo) {
-        // Chama o método bloqueia da classe Livro passando o usuário (this) e o prazo
         return it.bloqueia(this, prazo);
     }
 
-    // Método para desbloquear um livro
+   
     public boolean desbloqueiaLivro(Livro it) {
-        // Chama o método desbloqueia da classe Livro passando o usuário (this)
         return it.desbloqueia(this);
     }
 
-    // Reescrita do método getCotaMaxima da classe base Usuario
+  
     @Override
     public int getCotaMaxima() {
-        return 5; // Professores têm uma cota máxima de 5 livros
+        return 5; 
     }
 
-    // Reescrita do método getPrazoMaximo da classe base Usuario
+ 
     @Override
     public int getPrazoMaximo() {
-        return 14; // Professores têm um prazo máximo de 14 dias para devolver livros
+        return 14; 
     }
 
-    // Reescrita do método isProfessor da classe base Usuario
     @Override
     public boolean isProfessor() {
-        return true; // Este método retorna true para indicar que o usuário é um professor
+        return true;
     }
 
-    // Reescrita do método toString da classe base Usuario
+ 
     @Override
     public String toString() {
-        return "Prof. " + getNome(); // Retorna uma representação em string do professor com o prefixo "Prof."
+        return "Prof. " + getNome(); 
     }
 }
 
